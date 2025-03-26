f4168e58aeeaf96be665e44d3c42053ee79daa6f
nebudev14 <wyun2006@gmail.com>
2025-03-26 03:15:45 -0400

    deployed

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Warren Yun';
+export const SITE_DESCRIPTION = 'Hi, I\'m Warren!';
