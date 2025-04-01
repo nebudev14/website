eb8fa5da4798aac5ddaa5b16d0b119da1ac127b3
nebudev14 <wyun2006@gmail.com>
2025-04-01 06:56:43 -0400

    SEO updates

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Warren Yun';
-export const SITE_DESCRIPTION = 'Hi, I\'m Warren!';
+export const SITE_DESCRIPTION = 'I\'m Warren, an undergraduate at WPI. I like robotics, machine learning, and a whole bunch of other things that are tanegntially related.';
